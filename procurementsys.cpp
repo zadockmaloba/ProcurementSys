cf631f57d54e2e05dfb61642d5dcc27847e3a6f2
carter zadock <carterzadock@outlook.com>
2020-08-18 20:03:40 +0300

    Add project files.

---
+++ procurementsys.cpp
@@ -0,0 +1,38 @@
+#include "procurementsys.h"
+
+ProcurementSys::ProcurementSys(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+    //nFile->open(QIODevice::ReadOnly);
+    //QString nStyle = QLatin1String( nFile->readAll() );
+    this->setAttribute(Qt::WA_TranslucentBackground);
+    this->setWindowOpacity(.978);
+    this->setStyleSheet("background-color: #00FFFFFF;");
+    this->setWindowFlags(Qt::FramelessWindowHint);
+    this->setMouseTracking(true);
+    //this->setStyleSheet(nStyle);
+    //qDebug() << nStyle;
+}
+
+ProcurementSys::~ProcurementSys()
+{
+    delete(nFile);
+}
+
+void ProcurementSys::mouseMoveEvent (QMouseEvent* evnt)
+{
+    qDebug() << evnt->pos();
+    if (evnt->buttons() & Qt::LeftButton)
+    {
+        qDebug() << "Left Button !";
+        QPoint npos = evnt->pos();
+        QPoint diff = npos - mpos;
+        this->move(this->pos() + diff);
+    }
+}
+
+void ProcurementSys::mousePressEvent(QMouseEvent* evnt)
+{
+    mpos = evnt->pos();
+}
---
+++ procurementsys.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_procurementsys.h"
+#include <qfile.h>
+#include <qdebug.h>
+#include <qmouseeventtransition.h>
+#include <qevent.h>
+
+class ProcurementSys : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ProcurementSys(QWidget *parent = Q_NULLPTR);
+    ~ProcurementSys();
+    void mouseMoveEvent(QMouseEvent * evnt) override ;
+    void mousePressEvent(QMouseEvent* evnt) override;
+
+private:
+    Ui::ProcurementSysClass ui;
+    QFile* nFile = new QFile("qss/zedqss/zed.qss");
+    QPoint mpos;
+};
