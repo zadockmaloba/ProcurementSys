682b078db8a1eb62cc8c4a6a3524a80d83ed33a2
carter zadock <carterzadock@outlook.com>
2020-09-13 23:27:40 +0300

    Fixed some memory deallocation issues

--- cashierapp.cpp
+++ cashierapp.cpp
@@ -5,11 +5,40 @@ CashierApp::CashierApp(QWidget *parent)
 {
 	ui.setupUi(this);
 	this->setAttribute(Qt::WA_DeleteOnClose);
+	connect(ui.pushButton, SIGNAL(pressed()), this, SLOT(createNewBill()));
 }
 
 CashierApp::~CashierApp()
 {
+	delete(&ui);
+	delete(newBill);
+	delete(this);
+	delete(ui.directCompos->layout());
+}
 
+void CashierApp::editBill()
+{
+}
+
+void CashierApp::clearBill()
+{
+}
+
+void CashierApp::mergeBill()
+{
+}
+
+void CashierApp::voidBill()
+{
+}
+
+void CashierApp::createNewBill()
+{
+	//newBill->setParent(ui.directCompos);
+	ui.directCompos->setLayout(new QGridLayout);
+	ui.directCompos->layout()->addWidget(newBill);
+	
+	//newBill->show();
 }
 
 void CashierApp::closeTab(QWidget* prntTab)
--- cashierapp.h
+++ cashierapp.h
@@ -12,7 +12,17 @@ public:
 	CashierApp(QWidget *parent = Q_NULLPTR);
 	~CashierApp();
 
+public slots:
+	void createNewBill();
+	void editBill();
+	void clearBill();
+	void mergeBill();
+	void voidBill();
+
 private:
 	Ui::CashierApp ui;
 	void closeTab(QWidget * prntTab);
+
+private:
+	csrNewBill* newBill = new csrNewBill();
 };
